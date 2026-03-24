bb7e682ad1aee542f375382d73ccb26869a43f8e
Katherine Bullard <katherinebullard1@icloud.com>
2026-03-24 01:26:47 +0000

    Added in code for preventing collision with wall, self and opponents

--- index.js
+++ index.js
@@ -67,30 +67,97 @@ function move(gameState) {
   }
 
   // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-  // boardWidth = gameState.board.width;
-  // boardHeight = gameState.board.height;
+  const boardWidth = gameState.board.width;
+  const boardHeight = gameState.board.height;
+
+  //left wall -> x = 0
+  //righht wall -> x = boardWidth - 1
+  //top wall -> y = boardHeight - 1
+  //bottom wall -> y = 0
+
+   if (myHead.x == 0) { //If my head is next to the left wall don't move left
+    isMoveSafe.left = false; 
+  }
+  else if (myHead.x == boardWidth - 1) { 
+    isMoveSafe.right = false;
+  }
+  else if (myHead.y == 0) {
+    isMoveSafe.down = false;
+  }
+  else if (myHead.y == boardHeight - 1){
+    isMoveSafe.up = false;
+  }
+
 
   // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-  // myBody = gameState.you.body;
+  const myBody = gameState.you.body;
+
+  //myHead.y vertical position of snake
+  //myHead.x horizontal position of snake
+  //segment.y vertical position of body segment
+  //segment.x horizontal position of body segment
+
+  if(myBody.length > 1){ //if the snake has more than just a head
+    for (let i = 1; i < myBody.length; i++){ 
+      const segment = myBody[i]; // each segment is one square of the body
+    if ((myHead.y - segment.y) === 1) {  //if the head is one square above a body segment
+      isMoveSafe.down = false;
+    }
+    if ((segment.y - myHead.y) === 1) { //if the head is one square below a body segment
+      isMoveSafe.up = false;
+    }
+    if ((myHead.x - segment.x) === 1) { //if the head is one square to the right of a body segment
+      isMoveSafe.left = false;
+    }
+    if ((segment.x - myHead.x) === 1) { //if the head is one square to the left of a body segment
+      isMoveSafe.right = false;
+       }
+    }
+  }
 
   // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-  // opponents = gameState.board.snakes;
+  const opponents = gameState.board.snakes;
 
-  // Are there any safe moves left?
-  const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
-  if (safeMoves.length == 0) {
-    console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-    return { move: "down" };
+  for (let i = 0; i < opponents.length; i++){
+    const opponent = opponents[i]; // each opponent is a snake
+    for (let j = 0; j < opponent.body.length; j++){
+      const segment = opponent.body[j]; // each segment is one square of the body
+      if ((myHead.y - segment.y) === 1) {  //if the head is one square above a body segment
+        isMoveSafe.down = false;
+      }
+      if ((segment.y - myHead.y) === 1) { //if the head is one square below a body segment
+        isMoveSafe.up = false;
+      }
+      if ((myHead.x - segment.x) === 1) { //if the head is one square to the right of a body segment
+        isMoveSafe.left = false;
+      }
+      if ((segment.x - myHead.x) === 1) { //if the head is one square to the left of a body segment
+        isMoveSafe.right = false;
+      }
+    }
   }
 
   // Choose a random move from the safe moves
-  const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+  // const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+    // Are there any safe moves left?
+    //Use this as fallback
+    // const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
+    // if (safeMoves.length == 0) {
+    //   console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
+    //   return { move: "down" };
+    // }
 
-  // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-  // food = gameState.board.food;
+  // TODO: Step 4 - Use A* Pathfinding instead of random move
+  const food = gameState.board.food;
 
+  
+  
   console.log(`MOVE ${gameState.turn}: ${nextMove}`)
-  return { move: nextMove };
+//  return { move: nextMove };
+}
+
+function aStarPathfinding(start, end, board){
+  
 }
 
 runServer({
