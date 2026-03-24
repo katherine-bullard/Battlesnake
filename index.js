1ec432081c3d606170989082fac591d2a3224fa3
Katherine Bullard <katherinebullard1@icloud.com>
2026-03-24 20:47:59 +0000

    Re-wrote code for battle snake collision and fixed error

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function info() {
   return {
     apiversion: "1",
     author: "kbullard1",       // TODO: Your Battlesnake Username
-    color: "#6B8A9C", // TODO: Choose color
-    head: "default",  // TODO: Choose head
-    tail: "default",  // TODO: Choose tail
+    color: "#FFFF00", // TODO: Choose color
+    head: "all-seeing",  // TODO: Choose head
+    tail: "nr-booster",  // TODO: Choose tail
   };
 }
 
@@ -41,7 +41,12 @@ function end(gameState) {
 // Valid moves are "up", "down", "left", or "right"
 // See https://docs.battlesnake.com/api/example-move for available data
 function move(gameState) {
-
+  
+  const myHead = gameState.you.body[0];
+  const myNeck = gameState.you.body[1];
+  const boardWidth = gameState.board.width;
+  const boardHeight = gameState.board.height;
+  
   let isMoveSafe = {
     up: true,
     down: true,
@@ -49,10 +54,14 @@ function move(gameState) {
     right: true
   };
 
+  let moves = {
+    up: {x: myHead.x, y: myHead.y + 1},
+    down: {x: myHead.x, y: myHead.y - 1},
+    left: {x: myHead.x - 1, y: myHead.y},
+    right: {x: myHead.x + 1, y: myHead.y}
+  }
+  
   // We've included code to prevent your Battlesnake from moving backwards
-  const myHead = gameState.you.body[0];
-  const myNeck = gameState.you.body[1];
-
   if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
     isMoveSafe.left = false;
 
@@ -66,31 +75,8 @@ function move(gameState) {
     isMoveSafe.up = false;
   }
 
-  // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-  const boardWidth = gameState.board.width;
-  const boardHeight = gameState.board.height;
-
-  //left wall -> x = 0
-  //righht wall -> x = boardWidth - 1
-  //top wall -> y = boardHeight - 1
-  //bottom wall -> y = 0
-
-   if (myHead.x == 0) { //If my head is next to the left wall don't move left
-    isMoveSafe.left = false; 
-  }
-  else if (myHead.x == boardWidth - 1) { 
-    isMoveSafe.right = false;
-  }
-  else if (myHead.y == 0) {
-    isMoveSafe.down = false;
-  }
-  else if (myHead.y == boardHeight - 1){
-    isMoveSafe.up = false;
-  }
-
-
   // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-  const myBody = gameState.you.body;
+  const myBody = gameState.you.body; 
 
   //myHead.y vertical position of snake
   //myHead.x horizontal position of snake
@@ -116,7 +102,9 @@ function move(gameState) {
   }
 
   // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-  const opponents = gameState.board.snakes;
+  const opponents = gameState.board.snakes.filter(
+    s => s.id !== gameState.you.id
+  );
 
   for (let i = 0; i < opponents.length; i++){
     const opponent = opponents[i]; // each opponent is a snake
@@ -137,28 +125,54 @@ function move(gameState) {
     }
   }
 
-  // Choose a random move from the safe moves
-  // const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
-    // Are there any safe moves left?
-    //Use this as fallback
-    // const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
-    // if (safeMoves.length == 0) {
-    //   console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-    //   return { move: "down" };
-    // }
-
   // TODO: Step 4 - Use food fill instead of random move
  // const food = gameState.board.food;
+  let nextMove = null;
+  let bestScore = -1;
+
+  for (const move in moves) {
+
+    //prevent collision
+    const newPos = moves[move];
+    //if the new position is out of bounds then the move is not safe
+    if (
+      newPos.x < 0 || newPos.x >= boardWidth ||
+      newPos.y < 0 || newPos.y >= boardHeight
+    ) {
+      isMoveSafe[move] = false;
+    }
+    
+    if (!isMoveSafe[move]) continue;  // skip unsafe moves
+
+    // use foodFill to score this move
+    const spaceScore = floodFill(newPos, boardWidth, boardHeight, myBody, opponents);
+
+     if (spaceScore >= bestScore) {
+      bestScore = spaceScore;
+      nextMove = move;
+    }
+  }
+
+  if (!nextMove) {
+    // Choose a random move from the safe moves
+    const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
+    if (safeMoves.length > 0) {
+      nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+    } else {
+      return { move: "down" };
+    }
+  }
   
   console.log(`MOVE ${gameState.turn}: ${nextMove}`)
-//  return { move: nextMove };
+  return { move: nextMove };
 }
 
-function foodFill(start, boardWidth, boardHeight, myBody, opponents){
+//The idea is to find open space vs occupied space with 4 way fill
+function floodFill(start, boardWidth, boardHeight, myBody, opponents){
     let queue = [start];  //queue of nodes to visit
     let visited =  []; //tracked visited nodes
     const key = (pos) => `${pos.x},${pos.y}`;  //key for the node
-    queue.push(start);
+   
     // While Q is not empty:
     while (queue.length > 0){
       const current = queue.shift(); //remove first element from queue
@@ -187,7 +201,10 @@ function foodFill(start, boardWidth, boardHeight, myBody, opponents){
        //boardHeight - 1 = top wall of board
 
         //if the neighbor wall is left, right, top, or bottom wall then skip
-        if(neighbor.x == 0 || neighbor.x == boardWidth - 1 || neighbor.y == 0 ||neighbor.y == boardHeight - 1){
+        if (
+          neighbor.x < 0 || neighbor.x >= boardWidth ||
+          neighbor.y < 0 || neighbor.y >= boardHeight
+        ) {
           continue;
         }
 
@@ -227,6 +244,8 @@ function foodFill(start, boardWidth, boardHeight, myBody, opponents){
       }
         
     }
+  
+   return visited.length; // total reachable open space
 
   }
 
